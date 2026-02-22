7ffce4defcaeb8542306e66872746f379f2b9134
kevinpshan <kevinpshan@gmail.com>
2026-02-22 17:42:42 -0500

    Delete test/counter.js

--- test/counter.js
+++
@@ -1,9 +0,0 @@
-function loadReport() {
-    const logs = JSON.parse(localStorage.getItem(KEY_LOGS) || "[]");
-    document.getElementById('reportLog').innerText = logs.length ? logs.join('\n') : "No entries recorded.";
-}
-
-function copyReport() {
-    navigator.clipboard.writeText(document.getElementById('reportLog').innerText);
-    alert("Report Copied");
-}
